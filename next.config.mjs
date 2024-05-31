20c2e049801bd9252b8ce9fa4e76dbcc9f7d8bf8
Furkan Ayhan <halimayhan25@gmail.com>
2024-05-30 22:27:32 -0700

    changed improve page ui

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [   
+            {
+            protocol: 'https',
+            hostname: 'i.giphy.com',
+            port: ''
+            }
+        ]
+    }
+};
 
 export default nextConfig;
--- src/app/improve/page.tsx
+++ src/app/improve/page.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import { useChat } from 'ai/react';
-import Rating from '@mui/material/Rating'
 import { useState } from 'react';
+
+import Rating from '@mui/material/Rating';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+import Card from 'react-bootstrap/Card'
+
+import Image from 'next/image';
+
+import styles from './page.module.css';
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   const [firstq, setFirstq] = useState(true);
@@ -13,70 +19,146 @@ export default function Chat() {
   const [thirdq, setThirdq] = useState(false);
   const [fourthq, setFourthq] = useState(false);
   const [fifthq, setFifthq] = useState(false);
-  function disableFirstQ (){
+
+  function disableFirstQ() {
     setFirstq(false);
     setSecondq(true);
-  } 
-  function disableSecondQ (){
+  }
+
+  function disableSecondQ() {
     setSecondq(false);
     setThirdq(true);
-  } 
-  function disableThirdQ (){
+  }
+
+  function disableThirdQ() {
     setThirdq(false);
     setFourthq(true);
   }
-  function disableFourthQ (){
+
+  function disableFourthQ() {
     setFourthq(false);
     setFifthq(true);
   }
-  function disableFifthQ (){
+
+  function disableFifthQ() {
     setFifthq(false);
   }
 
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-
-        {firstq && <form onSubmit={disableFirstQ}>
-            <h1>Rate your shooting from 0 to 10</h1>
-            <Rating precision = {0.5} name="customized-10" defaultValue={0} max={10} />
-            <Button type='submit'>Next</Button>
-        </form>}
-
-        {secondq && <form onSubmit={disableSecondQ}>
-            <h1>Rate your passing from 0 to 10</h1>
-            <Rating precision = {0.5} name="customized-10" defaultValue={0} max={10} />
-            <Button type = 'submit'>Next</Button>            
-        </form>}
-        {thirdq && <form onSubmit={disableThirdQ}>
-            <h1>Rate your dribbling from 0 to 10</h1>
-            <Rating precision = {0.5} name="customized-10" defaultValue={0} max={10} />
-            <Button type = 'submit'>Next</Button>            
-        </form>}
-        {fourthq && <form onSubmit={disableFourthQ}>
-            <h1>Rate your speed from 0 to 10</h1>
-            <Rating precision = {0.5} name="customized-10" defaultValue={0} max={10} />
-            <Button type = 'submit'>Next</Button>            
-        </form>}
-        {fifthq && <form onSubmit={disableFifthQ}>
-            <h1>Summarize your play style</h1>
-            <input placeholder = "Summary"/>
-            <Button type = 'submit'>Next</Button>            
-        </form>}
-        
-        {messages.map(m => (
-            <div key={m.id} className="whitespace-pre-wrap">
-                {/* {m.role === 'user' ? 'User: ' : 'AI: '} */}
-                {m.content}
+    <div className={`container ${styles.chatContainer}`}>
+        {firstq && (
+          
+          <Form onSubmit={disableFirstQ} className="mb-3">
+            <Card className="text-center">
+            <Card.Title><h4 className="font-bold text-large">Rate your shooting from 0 to 10</h4></Card.Title>
+            <div className={styles.carouselImageContainer}>
+              <Image 
+                src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjZzbDFvemN2MjBzdzNhN3VtZ2hjYTMxNjh5Y3E3dHVoeTBzYzE1ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Y1MnfWIvQUIXq6Apia/giphy.gif" 
+                alt='Shooting image' 
+                layout="fill" 
+                objectFit="cover"
+                quality={100}
+              />
             </div>
-        ))}
-        <form onSubmit={handleSubmit}>
-            <input
-            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+
+            <Rating precision={0.5} name="customized-10" defaultValue={0} max={10} />
+            <Button type="submit" className="mt-3">Next</Button>
+            </Card>
+          </Form>
+        )}
+
+        {secondq && (
+          <Form onSubmit={disableSecondQ} className="mb-3">
+            <Card className="text-center">
+            <Card.Title>Rate your passing from 0 to 10</Card.Title>
+            <div className={styles.carouselImageContainer}>
+              <Image 
+                src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGxidGV1NG55NW1pa24zNGdiMzEzMWw2NzR3ODRwcGllc3c2ZTR2YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vnopV0LmQp9OFV9ibX/giphy.gif" 
+                alt='Passing image' 
+                layout="fill" 
+                objectFit="cover"
+                quality={100}
+              />
+            </div>
+            <Rating precision={0.5} name="customized-10" defaultValue={0} max={10} />
+            <Button type="submit" className="mt-3">Next</Button>
+            </Card>
+          </Form>
+        )}
+
+        {thirdq && (
+          <Form onSubmit={disableThirdQ} className="mb-3">
+            <Card className="text-center">
+            <Card.Title>Rate your dribbling from 0 to 10</Card.Title>
+            <div className={styles.carouselImageContainer}>
+              <Image 
+                src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2FjbnR3aXdpdnVvaGY2eXAzOXFjemdueGZ1MnJ2OWc5b3lhMTBhNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/k5fpeWhuGsQhEEOinT/giphy-downsized-large.gif" 
+                alt='Dribbling image' 
+                layout="fill" 
+                objectFit="cover"
+                quality={100}
+              />
+            </div>
+            <Rating precision={0.5} name="customized-10" defaultValue={0} max={10} />
+            <Button type="submit" className="mt-3">Next</Button>
+            </Card>
+          </Form>
+        )}
+
+        {fourthq && (
+          <Form onSubmit={disableFourthQ} className="mb-3">
+            <Card className="text-center">
+            <Card.Title>Rate your speed from 0 to 10</Card.Title>
+            <div className={styles.carouselImageContainer}>
+              <Image 
+                src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZG5ocGl2ZThiZGZmdnFmcDc2YWZybXZxbXF0MHR4NXd1aXJyNXcxNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2t9oQRS8lBgVZCard.TitleE0t/giphy.gif" 
+                alt='Passing image' 
+                layout="fill" 
+                objectFit="cover"
+                quality={100}
+              />
+            </div>
+            <Rating precision={0.5} name="customized-10" defaultValue={0} max={10} />
+            <Button type="submit" className="mt-3">Next</Button>
+            </Card>
+          </Form>
+        )}
+
+        {fifthq && (
+          <Form onSubmit={disableFifthQ} className="mb-3">
+            <Card className="text-center">
+            <Card.Title>Summarize your play style</Card.Title>
+            <div className={styles.carouselImageContainer}>
+              <Image 
+                src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGxidGV1NG55NW1pa24zNGdiMzEzMWw2NzR3ODRwcGllc3c2ZTR2YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vnopV0LmQp9OFV9ibX/giphy.gif" 
+                alt='Passing image' 
+                layout="fill" 
+                objectFit="cover"
+                quality={100}
+              />
+            </div>
+            <Form.Control type="text" placeholder="Summary" className="mb-3" />
+            <Button type="submit">Next</Button>
+            </Card>
+          </Form>
+        )}
+
+        <div className="mt-5">
+          {messages.map((m) => (
+            <div key={m.id}>
+              {m.content}
+            </div>
+          ))}
+        </div>
+
+        {/* <Form onSubmit={handleSubmit} className="fixed-bottom p-3 bg-light border-top">
+          <Form.Control
+            className="w-100 p-2"
             value={input}
             placeholder="Say something..."
             onChange={handleInputChange}
-            />
-        </form>
+          />
+        </Form> */}
     </div>
   );
 }
