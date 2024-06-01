fbd31acfb99553c42c17e099ee993db6cb7ef0db
Furkan Ayhan <halimayhan25@gmail.com>
2024-06-01 01:23:17 -0700

    changed the component a bit and added new features to improve page

--- src/app/improve/components/questions.tsx
+++ src/app/improve/components/questions.tsx
@@ -1,22 +1,46 @@
 'use client';
-
+import { useState } from 'react';
 import Rating from '@mui/material/Rating';
 import Button from 'react-bootstrap/Button';
-
+import Form from 'react-bootstrap/Form'
 
 import Card from 'react-bootstrap/Card'
 
 import Image from 'next/image';
 
 import styles from './questions.module.css'
+import { set } from 'zod';
+import { Input } from '@mui/material';
+
+let ratingsMap = new Map();
+ratingsMap.set("shooting", 0);
+ratingsMap.set("passing", 0);
+ratingsMap.set("dribbling", 0);
+ratingsMap.set("speed", 0);
+ratingsMap.set("play style", "")
+
 export default function Questions(props:any){
+  const [value,setValue] = useState<number | null>(2);
+  const [summaryVal, setSummaryVal] = useState("")
     function Capitalize(str:string){
         return str.charAt(0).toUpperCase() + str.slice(1);
-        }
+    }
+    
+    function consoleCheck (){
+      if (props.title != "play style"){
+        {ratingsMap.set(props.title,value)}
+      }
+      else{
+        {ratingsMap.set(props.title,summaryVal)}
+      }
+      console.log(ratingsMap);
+    }
+
     return (
-        <Card className="text-center">
+        
+        props.title === "play style" ? <Card bg = "Success" className="text-center">
         <Card.Title>{Capitalize(props.title)}</Card.Title>
-        <Card.Text>Rate your {props.title} from 0 to 10</Card.Text>
+        <Card.Text>Summarize your {props.title}</Card.Text>
         <div className={styles.carouselImageContainer}>
         
           <Image 
@@ -28,9 +52,39 @@ export default function Questions(props:any){
           />
         
         </div>
-        <Card.Text>Click on the star that you think shows your level</Card.Text>
-        <Rating precision={0.5} name="customized-10" defaultValue={0} max={10} style={{paddingLeft:"27%"}} />
-        <Button type="submit" className="mt-3">Next</Button>
+        <Form>
+          <Form.Control 
+            value={summaryVal} 
+            as="textarea" 
+            placeholder="Summarize" 
+            rows={2}
+          />
+        </Form>
+      
+        <Button onClick = {consoleCheck} type="submit" className="mt-3">Submit</Button>
+      </Card>:
+      <Card bg = "Success" className="text-center">
+          <Card.Title>{Capitalize(props.title)}</Card.Title>
+          <Card.Text>Rate your {props.title} from 0 to 10</Card.Text>
+          <div className={styles.carouselImageContainer}>
+          
+            <Image 
+              src= {props.src} 
+              alt={`${props.title} image`} 
+              layout='fill'
+              objectFit='cover'
+              quality={100}
+            />
+          
+          </div>
+          <Card.Text>Click on the star that you think shows your level</Card.Text>
+          <Rating precision={0.5} name="customized-10" defaultValue={0} max={10} style={{paddingLeft:"27%"}} 
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);                
+              }}
+          />
+          <Button onClick = {consoleCheck} type="submit" className="mt-3">Next</Button>
         </Card>
     )
 }
--- src/app/improve/page.tsx
+++ src/app/improve/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useChat } from 'ai/react';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 import Form from 'react-bootstrap/Form';
 
@@ -10,85 +10,50 @@ import Questions from './components/questions';
 import styles from './page.module.css';
 
 
+const questions = [
+  {
+    title: "shooting",
+    src: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjZzbDFvemN2MjBzdzNhN3VtZ2hjYTMxNjh5Y3E3dHVoeTBzYzE1ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Y1MnfWIvQUIXq6Apia/giphy.gif"
+  },
+  {
+    title: "passing",
+    src: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGxidGV1NG55NW1pa24zNGdiMzEzMWw2NzR3ODRwcGllc3c2ZTR2YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vnopV0LmQp9OFV9ibX/giphy.gif"
+  },
+  {
+    title: "dribbling",
+    src: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2FjbnR3aXdpdnVvaGY2eXAzOXFjemdueGZ1MnJ2OWc5b3lhMTBhNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/k5fpeWhuGsQhEEOinT/giphy-downsized-large.gif"
+  },
+  {
+    title: "speed",
+    src: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHk5bm0waGhuemhiMW45dHdnZGpmNHQ5NGZjeDFwMWl1czNnZGN1biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/H7WGljzHQmOY132mSi/giphy.gif"
+  },
+  {
+    title: "play style",
+    src: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGxidGV1NG55NW1pa24zNGdiMzEzMWw2NzR3ODRwcGllc3c2ZTR2YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vnopV0LmQp9OFV9ibX/giphy.gif"
+  }
+];
+
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
-  const [firstq, setFirstq] = useState(true);
-  const [secondq, setSecondq] = useState(false);
-  const [thirdq, setThirdq] = useState(false);
-  const [fourthq, setFourthq] = useState(false);
-  const [fifthq, setFifthq] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
-  function disableFirstQ() {
-    setFirstq(false);
-    setSecondq(true);
-  }
 
-  function disableSecondQ() {
-    setSecondq(false);
-    setThirdq(true);
+  const handleNextQuestion = (event:any) => {
+    event.preventDefault();
+    setCurrentQuestion(prev => prev + 1);
   }
-
-  function disableThirdQ() {
-    setThirdq(false);
-    setFourthq(true);
-  }
-
-  function disableFourthQ() {
-    setFourthq(false);
-    setFifthq(true);
-  }
-
-  function disableFifthQ() {
-    setFifthq(false);
-  }
+  
 
   return (
     <div className={`container ${styles.chatContainer}`}>
-        {firstq && (
-          
-          <Form onSubmit={disableFirstQ} className="mb-3">
-            <Questions 
-              title = "shooting"
-              src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjZzbDFvemN2MjBzdzNhN3VtZ2hjYTMxNjh5Y3E3dHVoeTBzYzE1ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Y1MnfWIvQUIXq6Apia/giphy.gif"
-            />
-          </Form>
-        )}
-
-        {secondq && (
-          <Form onSubmit={disableSecondQ}>
-            <Questions 
-              title = "passing"
-              src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGxidGV1NG55NW1pa24zNGdiMzEzMWw2NzR3ODRwcGllc3c2ZTR2YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vnopV0LmQp9OFV9ibX/giphy.gif" 
-            />
-          </Form>
-        )}
-
-        {thirdq && (
-          <Form onSubmit={disableThirdQ} className="mb-3">
-            <Questions
-              title = "dribbling"
-              src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2FjbnR3aXdpdnVvaGY2eXAzOXFjemdueGZ1MnJ2OWc5b3lhMTBhNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/k5fpeWhuGsQhEEOinT/giphy-downsized-large.gif" 
-            />
-          </Form>
-        )}
-
-        {fourthq && (
-          <Form onSubmit={disableFourthQ} className="mb-3">
-            <Questions
-              title = "speed"
-              src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHk5bm0waGhuemhiMW45dHdnZGpmNHQ5NGZjeDFwMWl1czNnZGN1biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/H7WGljzHQmOY132mSi/giphy.gif" 
-            />
-          </Form>
-        )}
-
-        {fifthq && (
-          <Form onSubmit={disableFifthQ} className="mb-3">
-            <Questions
-              title = "play style"
-              src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGxidGV1NG55NW1pa24zNGdiMzEzMWw2NzR3ODRwcGllc3c2ZTR2YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vnopV0LmQp9OFV9ibX/giphy.gif" 
-            />
-          </Form>
-        )}
+      {currentQuestion < questions.length && (
+        <Form onSubmit={handleNextQuestion} className="mb-3">
+          <Questions 
+            title={questions[currentQuestion].title}
+            src={questions[currentQuestion].src}
+          />
+        </Form>
+      )}
 
         {/* <div className="mt-5">
           {messages.map((m) => (
